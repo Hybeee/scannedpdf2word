66e7ff2909d8097971a3f22d130f0fdca5b40f39
Hybeee <szcsa2003@gmail.com>
2024-08-07 08:38:37 +0200

    update

---
+++ randommm.py
@@ -0,0 +1,4 @@
+from docx2pdf import convert
+
+# Replace 'your-document.docx' with the path to your Word document
+convert("bovitett_absztrakt_result\\bovitett_absztrakt_converted.docx", "bovitett_absztrakt_result\\bovitett_absztrakt_converted.pdf")
